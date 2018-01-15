fe87686a0d48d15f41fb4567c52d7e784e8c3db0
oriash93 <oriash93@gmail.com>
2018-01-15 21:18:14 +0200

    Moved database folder into app, updated scripts

--- app/database.js
+++ app/database.js
@@ -1,6 +1,6 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('scraper/data.json');
+const adapter = new FileSync('app/scraper/data.json');
 const db = low(adapter);
 
 db.defaults({
